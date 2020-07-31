bd9a635c882f8b46b2fa92fbf86e99bbc131352a
Roberto Gutierrez <roberm98a@gmail.com>
2020-07-31 12:25:14 -0600

    Changes to run yolo

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,9 +1,9 @@
-model=butterfly-tiny-yolov4 #model inside models folder
+model=model2 #model inside models folder
 weight_id=last
 
-cfg=../models/${model}/butterfly-tiny-yolov4.cfg
+cfg=../models/${model}/butterfly-ty4.cfg
 data=../models/${model}/data/obj.data
-weights=../models/${model}/backup/butterfly-tiny-yolov4_${weight_id}.weights
+weights=../models/other/weights/yolov4-tiny.conv.29
 log=../models/${model}/logs/log_train.txt
 
-../darknet detector train ${data} ${cfg} ${weights} -map | tee ${log} --append
+../darknet detector train ${data} ${cfg} ${weights} -dont_show | tee ${log} --append
